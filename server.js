a7b87a186d2a53e60051da94d2c9cef3df04dc8f
chrisplmmr <chrisplummer68@gmail.com>
2022-10-25 10:42:34 -0500

    Changed data folder

--- server.js
+++ server.js
@@ -11,127 +11,127 @@ app.get('/express_backend', (req, res) => {
 });
 
 // app.get('/2019data', (_, res) => {
-// 	res.sendFile(__dirname + '\\routes\\2019.txt');
+// 	res.sendFile(__dirname + '\\datafiles\\2019.txt');
 // });
 
 app.get('/2019data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2019.txt');
+	res.sendFile(__dirname + '\\datafiles\\2019.txt');
 });
 
 app.get('/2018data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2018.txt');
+	res.sendFile(__dirname + '\\datafiles\\2018.txt');
 });
 
 app.get('/2017data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2017.txt');
+	res.sendFile(__dirname + '\\datafiles\\2017.txt');
 });
 
 app.get('/2016data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2016.txt');
+	res.sendFile(__dirname + '\\datafiles\\2016.txt');
 });
 
 app.get('/2015data', (_, res) => {
-res.sendFile(__dirname + '\\routes\\2015.txt');
+res.sendFile(__dirname + '\\datafiles\\2015.txt');
 });
 
 app.get('/2014data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2014.txt');
+	res.sendFile(__dirname + '\\datafiles\\2014.txt');
 });
 
 app.get('/2013data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2013.txt');
+	res.sendFile(__dirname + '\\datafiles\\2013.txt');
 });
 
 app.get('/2012data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2012.txt');
+	res.sendFile(__dirname + '\\datafiles\\2012.txt');
 });
 
 app.get('/2011data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2011.txt');
+	res.sendFile(__dirname + '\\datafiles\\2011.txt');
 });
 
 app.get('/2010data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2010.txt');
+	res.sendFile(__dirname + '\\datafiles\\2010.txt');
 });
 
 app.get('/2009data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2009.txt');
+	res.sendFile(__dirname + '\\datafiles\\2009.txt');
 });
 
 app.get('/2008data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2008.txt');
+	res.sendFile(__dirname + '\\datafiles\\2008.txt');
 });
 
 app.get('/2007data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2007.txt');
+	res.sendFile(__dirname + '\\datafiles\\2007.txt');
 });
 
 app.get('/2006data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2006.txt');
+	res.sendFile(__dirname + '\\datafiles\\2006.txt');
 });
 
 app.get('/2005data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2005.txt');
+	res.sendFile(__dirname + '\\datafiles\\2005.txt');
 });
 
 app.get('/2004data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2004.txt');
+	res.sendFile(__dirname + '\\datafiles\\2004.txt');
 });
 
 app.get('/2003data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2003.txt');
+	res.sendFile(__dirname + '\\datafiles\\2003.txt');
 });
 
 app.get('/2002data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2002.txt');
+	res.sendFile(__dirname + '\\datafiles\\2002.txt');
 });
 
 app.get('/2001data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2001.txt');
+	res.sendFile(__dirname + '\\datafiles\\2001.txt');
 });
 
 app.get('/2000data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\2000.txt');
+	res.sendFile(__dirname + '\\datafiles\\2000.txt');
 });
 
 app.get('/1999data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1999.txt');
+	res.sendFile(__dirname + '\\datafiles\\1999.txt');
 });
 
 app.get('/1998data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1998.txt');
+	res.sendFile(__dirname + '\\datafiles\\1998.txt');
 });
 
 app.get('/1997data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1997.txt');
+	res.sendFile(__dirname + '\\datafiles\\1997.txt');
 });
 
 app.get('/1996data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1996.txt');
+	res.sendFile(__dirname + '\\datafiles\\1996.txt');
 });
 
 app.get('/1995data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1995.txt');
+	res.sendFile(__dirname + '\\datafiles\\1995.txt');
 });
 
 app.get('/1994data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1994.txt');
+	res.sendFile(__dirname + '\\datafiles\\1994.txt');
 });
 
 app.get('/1993data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1993.txt');
+	res.sendFile(__dirname + '\\datafiles\\1993.txt');
 });
 
 app.get('/1992data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1992.txt');
+	res.sendFile(__dirname + '\\datafiles\\1992.txt');
 });
 
 app.get('/1991data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1991.txt');
+	res.sendFile(__dirname + '\\datafiles\\1991.txt');
 });
 
 app.get('/1990data', (_, res) => {
-	res.sendFile(__dirname + '\\routes\\1990.txt');
+	res.sendFile(__dirname + '\\datafiles\\1990.txt');
 });
 
 // Routes handled in react
